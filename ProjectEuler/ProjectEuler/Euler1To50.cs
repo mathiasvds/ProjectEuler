431409712fc2cfb56304005b4b0e65345e0c8d62
Mathias <mathias.vanderstock@gmail.com>
2019-05-28 23:18:01 +0200

    - multiplesof3and5

---
+++ ProjectEuler/ProjectEuler/Euler1To50.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public class Euler1To50
+    {
+        public int MultiplesOf3And5()
+        {
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 5 == 0 || i % 3 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var euler = new Euler1To50();
+
+            Console.WriteLine(euler.MultiplesOf3And5());
+            Console.ReadKey();
+        }
+    }
+}
